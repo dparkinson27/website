8e022c22d1b4fd884dd8c3999de40cb236c2f2d7
dparkinson27 <33329497+dparkinson27@users.noreply.github.com>
2019-10-06 21:29:57 -0500

    Add files via upload

---
+++ artistsite/js/drawingimagemanager.js
@@ -0,0 +1,38 @@
+/* 
+ * Creates artPiece object that stores all variables for
+ * image creation on DOM
+ */
+function artPiece (source, width, height, desc) {
+    var figure = document.createElement("div");
+    var image = document.createElement("img");
+    image.src = source;
+    image.width = width;
+    image.height = height;
+    image.alt = desc;
+    image.className = "Imagelist";
+    var caption = document.createElement("div");
+    caption.textContent = desc;
+    caption.className = "label";
+    figure.appendChild(image);
+    figure.appendChild(caption);
+    this.showImage = function(id) {
+        var add = document.getElementById(id);
+        add.appendChild(figure); 
+    };
+}
+//Calls in JSON file for images
+var db = new XMLHttpRequest();
+db.onload = function() {
+    if(db.status === 200) {
+        pieces = JSON.parse(db.responseText);
+        
+        for(var i = 0; i < pieces.artPiece.length; i++){
+            var tba = new artPiece(pieces.artPiece[i].source, pieces.artPiece[i].width, pieces.artPiece[i].height, pieces.artPiece[i].description);
+            tba.showImage("drawingrow");
+        }
+        
+    }
+};
+db.open('GET', "drawing.json", true);
+db.send(null);
+
---
+++ artistsite/js/paintingimagemanager.js
@@ -0,0 +1,37 @@
+/* 
+ * See drawingimagemanager for documentation
+ */
+function artPiece (source, width, height, desc) {
+    var figure = document.createElement("div");
+    var image = document.createElement("img");
+    image.src = source;
+    image.width = width;
+    image.height = height;
+    image.alt = desc;
+    image.className = "Imagelist";
+    var caption = document.createElement("div");
+    caption.textContent = desc;
+    caption.className = "label";
+    figure.appendChild(image);
+    figure.appendChild(caption);
+    this.showImage = function(id) {
+        var add = document.getElementById(id);
+        add.appendChild(figure); 
+    };
+}
+
+var db = new XMLHttpRequest();
+db.onload = function() {
+    if(db.status === 200) {
+        pieces = JSON.parse(db.responseText);
+        
+        for(var i = 0; i < pieces.artPiece.length; i++){
+            var tba = new artPiece(pieces.artPiece[i].source, pieces.artPiece[i].width, pieces.artPiece[i].height, pieces.artPiece[i].description);
+            tba.showImage("paintingrow");
+        }
+        
+    }
+};
+db.open('GET', "painting.json", true);
+db.send(null);
+
